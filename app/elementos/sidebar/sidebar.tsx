b6389bb68ca802604ab1d9e7d840bee01477ff85
Admin MGO <167483251+MGO-ind@users.noreply.github.com>
2025-02-11 09:00:30 -0600

    Update sidebar.tsx

--- app/elementos/sidebar/sidebar.tsx
+++ app/elementos/sidebar/sidebar.tsx
@@ -1,20 +1,10 @@
 
-
-import { AccountToggle } from "./accountToggle";
-import { Plan } from "./plan";
-import { RouteSelect } from "./routeSelect";
-import { Search } from "./search";
-
 export default function SideBar() {
 
   return (
     <div className="font-[family-name:var(--font-geist-mono)]">
       <div className="overflow-y sticky top-4 h-[calc(100vh-32px-48px)]" >
-        <AccountToggle/>
-        <Search/>
-        <RouteSelect/>
-      </div>
-      <Plan/>
+       sidebar
     </div>
 
   );
