40baacab592cac8306f67efe544e3f8f18d06b8a
Admin MGO <167483251+MGO-ind@users.noreply.github.com>
2025-02-11 09:02:31 -0600

    Update sidebar.tsx

--- app/elementos/sidebar/sidebar.tsx
+++ app/elementos/sidebar/sidebar.tsx
@@ -4,7 +4,7 @@ export default function SideBar() {
   return (
     <div className="font-[family-name:var(--font-geist-mono)]">
       <div className="overflow-y sticky top-4 h-[calc(100vh-32px-48px)]" >
-       sidebar
+       sidebarasd
     </div>
 
   );
