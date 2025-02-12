676b8119acbf4292b29c3cb858a65cb2ba657818
Admin MGO <167483251+MGO-ind@users.noreply.github.com>
2025-02-12 10:48:23 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  /* ignore build errors */
-      ignoreBuildErrors: true,
-
-eslint:{
-    ignoreDuringBuilds: true, during builds
+  module.exports = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
   },
+},
  
 };
 
