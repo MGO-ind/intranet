33af849598fae5dd8b52b7b098d2f5d80d4a41ff
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-02-12 13:40:23 -0600

    asd

--- app/cotizador/page.tsx
+++ app/cotizador/page.tsx
@@ -1,9 +1,11 @@
 import { Cotizador } from "../elementos/cotizador/cotizador";
+import SideBar from "../elementos/sidebar/sidebar";
 
 export default function Home() {
     return (
       
       <main className="grid gap-4 p-4 grid-cols-[220px,_1fr]">
+        <SideBar/>
        <Cotizador/>
       </main>   
     );
