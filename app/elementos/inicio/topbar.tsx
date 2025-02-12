3589f686b2d4e8f7f17f698710fd08cea7ee07bc
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-02-12 13:36:20 -0600

    topb

--- app/elementos/inicio/topbar.tsx
+++ app/elementos/inicio/topbar.tsx
@@ -1,10 +1,25 @@
+import { FiCalendar } from "react-icons/fi";
+
 export const TopBar = () => {
 
   
   
   return (
     <div className="border-b px-4 mb-4 mt-2 pb-4 border-stone-200">
-     TopBar
+    <div className="flex items-center justify-between p-0.5">
+      <div>
+        <span className="text-sm font-bold block">🚀 Buen día, Name!</span>
+        <span className="text-xs block text-stone-200">
+          
+        </span>
+      </div>
+
+      <button className="flex text-sm items-center gap-2 text-stone-900 bg-stone-100 transition-colors hover:bg-blue-100 hover:text-blue-800 px-3 py-1.5 rounded-full">
+        <FiCalendar />
+        <span>Historial</span>
+      </button>
     </div>
-  );
+  </div>
+);
+
 };
