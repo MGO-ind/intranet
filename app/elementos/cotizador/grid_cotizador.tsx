9825fa9114ddd941875f2dc3a6f39412d0de60d9
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-02-12 12:43:44 -0600

    dolares

--- app/elementos/cotizador/grid_cotizador.tsx
+++ app/elementos/cotizador/grid_cotizador.tsx
@@ -1,9 +1,10 @@
+import { CambioDolares } from "./cambio_dolares";
 
 export const GridCotizador = () => {
     return (
       
       <main className="grid gap-4 p-4 grid-cols-[220px,_1fr]">
-        
+        <CambioDolares/>
       </main>   
     );
   }
