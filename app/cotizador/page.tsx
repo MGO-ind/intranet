dff528834b31458a366aed1bc30e47ded3d3e53e
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-02-12 12:51:50 -0600

    asd

--- app/cotizador/page.tsx
+++ app/cotizador/page.tsx
@@ -1,8 +1,10 @@
+import { Cotizador } from "../elementos/cotizador/cotizador";
+
 export default function Home() {
     return (
       
       <main className="grid gap-4 p-4 grid-cols-[220px,_1fr]">
-       
+       <Cotizador/>
       </main>   
     );
   }
--- app/elementos/inicio/dashboard.tsx
+++ app/elementos/inicio/dashboard.tsx
@@ -1,11 +1,12 @@
 "use client"
 
 import { Grid } from "./grid";
+import { TopBar } from "./topbar";
 
 export function Dashboard() {
     return(
         <div className="bg-zinc-700 rounded-3xl pb-4 shadow h-auto">
-            
+            <TopBar/>
             <Grid/>
   
             
