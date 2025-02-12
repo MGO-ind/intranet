6f85658d5e21f5f9d349562a16076065356fae2a
Admin MGO <167483251+MGO-ind@users.noreply.github.com>
2025-02-12 08:51:20 -0600

    Update dashboard.tsx

--- app/elementos/inicio/dashboard.tsx
+++ app/elementos/inicio/dashboard.tsx
@@ -1,1 +1,14 @@
+import React from "react";
+import { TopBar } from "./topBar";
+import { Grid } from "./grid";
 
+export function Dashboard() {
+    return(
+        <div className="bg-zinc-700 rounded-3xl pb-4 shadow h-auto">
+            <TopBar/>
+            <Grid/>
+            
+        </div>
+
+    );
+}
