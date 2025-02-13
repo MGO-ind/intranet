93959fea99f4e4eb5f4588da4a0aa97a6757c5ea
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-02-13 08:42:34 -0600

    colores

--- app/elementos/sidebar/sidebar.tsx
+++ app/elementos/sidebar/sidebar.tsx
@@ -3,7 +3,7 @@ export default function SideBar() {
 
   return (
 
-      <div className="overflow-y sticky top-4 h-[calc(100vh-32px-48px)]" >
+      <div className="overflow-y bg-black text-white sticky top-4 h-[calc(100vh-32px-48px)]" >
        sidebarasd
     </div>
 
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Dashboard } from "./elementos/inicio/dashboard";
 
 export default function Home() {
   return (
-    <main className="grid gap-4 p-4 grid-cols-[220px,_1fr]">
+    <main className="grid bg-black text-white gap-4 p-4 grid-cols-[220px,_1fr]">
       <SideBar />
       <Dashboard />
     </main> 
