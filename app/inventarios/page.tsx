e72d054669b16966a81e660b72af40e6d1434be4
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-02-13 16:43:29 -0600

    inventarios

--- app/inventarios/page.tsx
+++ app/inventarios/page.tsx
@@ -1,11 +1,11 @@
+import SideBar from "../elementos/sidebar/sidebar";
+
 export default function Inventarios() {
 
   return (
-
-      <div className="overflow-y sticky top-4 h-[calc(100vh-32px-48px)]" >
-       Inventario
-    </div>
-
-
+    <main className="grid gap-4 p-4 grid-cols-[220px,_1fr]">
+      <SideBar />
+ 
+    </main>
   );
 }
