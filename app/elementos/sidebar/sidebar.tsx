57af20ce8a8e82a2876cc4f95201cf9645eb7318
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-02-13 13:54:23 -0600

    rutas del menu

--- app/elementos/sidebar/sidebar.tsx
+++ app/elementos/sidebar/sidebar.tsx
@@ -1,13 +1,17 @@
 import { AccountToggle } from "./accounToggle";
+import { Plan } from "./plan";
+import { RouteSelect } from "./routeSelect";
 
 export default function SideBar() {
 
   return (
-
+<>
       <div className="overflow-y bg-black text-white sticky top-4 h-[calc(100vh-32px-48px)]" >
         <AccountToggle/>
-       sidebarasd
+       <RouteSelect/>
     </div>
+    <Plan/>
+    </>
 
 
   );
