0571db25058272f553ba5c1883e1270ba2ac66e1
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-02-13 15:43:29 -0600

    barra de busqueda

--- app/elementos/sidebar/sidebar.tsx
+++ app/elementos/sidebar/sidebar.tsx
@@ -1,6 +1,7 @@
 import { AccountToggle } from "./accounToggle";
 import { Plan } from "./plan";
 import { RouteSelect } from "./routeSelect";
+import { Search } from "./search";
 
 export default function SideBar() {
 
@@ -8,6 +9,7 @@ export default function SideBar() {
     <div className="font-[family-name:var(--font-geist-mono)]">
       <div className="overflow-y bg-black text-white sticky top-4 h-[calc(100vh-32px-48px)]" >
         <AccountToggle/>
+        <Search/>
        <RouteSelect/>
     </div>
     <Plan/>
