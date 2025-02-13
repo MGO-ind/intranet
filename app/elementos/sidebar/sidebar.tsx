a7f028ddb725182e4e094e1b7c4b8cc284d51e0f
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-02-13 13:45:36 -0600

    toggle account

--- app/elementos/sidebar/sidebar.tsx
+++ app/elementos/sidebar/sidebar.tsx
@@ -1,9 +1,11 @@
+import { AccountToggle } from "./accounToggle";
 
 export default function SideBar() {
 
   return (
 
       <div className="overflow-y bg-black text-white sticky top-4 h-[calc(100vh-32px-48px)]" >
+        <AccountToggle/>
        sidebarasd
     </div>
 
