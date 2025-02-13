3340238cfe327a11c2d961e1ff86ba2d8104ea3b
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-02-13 13:59:03 -0600

    asd

--- app/elementos/sidebar/sidebar.tsx
+++ app/elementos/sidebar/sidebar.tsx
@@ -5,13 +5,13 @@ import { RouteSelect } from "./routeSelect";
 export default function SideBar() {
 
   return (
-<>
+    <div className="font-[family-name:var(--font-geist-mono)]">
       <div className="overflow-y bg-black text-white sticky top-4 h-[calc(100vh-32px-48px)]" >
         <AccountToggle/>
        <RouteSelect/>
     </div>
     <Plan/>
-    </>
+    </div>
 
 
   );
