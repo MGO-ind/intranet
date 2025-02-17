ddee5bbe44e2c6f39c05891f0a6b7b4fe2463418
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-02-17 14:03:10 -0600

    a

--- app/elementos/inicio/grid.tsx
+++ app/elementos/inicio/grid.tsx
@@ -1,10 +1,12 @@
 import Footer from "../footer";
+import Page from "./tablaInicio";
 
 export const Grid = () => {
   return (
     <>
     <div className="px-4 grid gap-3 grid-cols-12">
     grid
+    <Page/>
     </div>
     <Footer/>
     </>
