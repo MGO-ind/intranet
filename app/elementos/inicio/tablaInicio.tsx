b750094016a0d4090aa8b38e1c553c233c711890
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-02-17 15:17:37 -0600

    asd

--- app/elementos/inicio/tablaInicio.tsx
+++ app/elementos/inicio/tablaInicio.tsx
@@ -2,18 +2,18 @@
 import { neon } from '@neondatabase/serverless';
 
 export default function Page() {
-  async function create(formData: FormData) {
+  /*async function create(formData: FormData) {
     'use server';
     // Connect to the Neon database
     const sql = neon(`${process.env.DATABASE_URL}`);
     const costofletes = formData.get('costofletes');
-    await sql('SELECT * FROM costofletes');
+    await sql('SELECT * FROM costofletes');*/
     // Insert the comment from the form into the Postgres database
     //await sql('INSERT INTO costofletes (costofletes) VALUES ($1)', [costofletes]);
-  }
+  //}
 
   return (
-    <form action={create}>
+    <form >
       <input type="text" placeholder="write a comment" name="comment" />
       <button type="submit">Submit</button>
     </form>
