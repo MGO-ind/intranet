20aaff2af2438fdd47c4a890ea51153fe691d39a
Nayeli Carrizales <nayeli.carrizales.diaz@gmail.com>
2025-02-18 09:36:29 -0600

    FormularioCostos

--- app/elementos/inicio/grid.tsx
+++ app/elementos/inicio/grid.tsx
@@ -1,12 +1,12 @@
 import Footer from "../footer";
-import {Page} from "./tablaInicio";
+import {FormularioCostos} from "./tablaInicio";
 
 export const Grid = () => {
   return (
     <>
     <div className="px-4 grid gap-3 grid-cols-12">
     grid
-    <Page/>
+    <FormularioCostos/>
     </div>
     <Footer/>
     </>
